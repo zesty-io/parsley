1a90b9581da45477c8079122bdd21fc9b168cf0c
Randy Apuzzo <apuzzo@gmail.com>
2021-04-15 21:57:50 -0700

    refactor

--- pvl/lib/ItemTypes.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var ItemTypes = exports.ItemTypes = {
-  LAYOUTOBJECT: 'LayoutObject',
-  LAYOUTOBJECTTEXT: 'LayoutObjectText'
-};
